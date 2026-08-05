b4716b05f01d6333c6c8853c69df344079e2bbc2
The-city-not-present <23407369+The-city-not-present@users.noreply.github.com>
2026-08-06 00:12:37 +0300

    Upd run.sh to match

--- run.sh
+++ run.sh
@@ -1,5 +1,30 @@
 #!/usr/bin/env bash
 set -e
 
-# python -m src.launcher --program gitgui --working-tree-folder "tests-real-sensitive-data/my-test-project" --git-repo-folder "tests-real-sensitive-data/test-project-repo"
-python dist/gitgui_bundle.py --program gitgui --working-tree-folder "tests-real-sensitive-data/my-test-project" --git-repo-folder "tests-real-sensitive-data/test-project-repo"
+
+echo "Prep python"
+echo "set venv"
+if [ -x ".venv/Scripts/python.exe" ]; then
+    pythonexecutable=".venv/Scripts/python.exe"
+elif [ -x ".venv/bin/python" ]; then
+    pythonexecutable=".venv/bin/python"
+else
+    python -m venv .venv
+    if [ -x ".venv/Scripts/python.exe" ]; then
+        pythonexecutable=".venv/Scripts/python.exe"
+    elif [ -x ".venv/bin/python" ]; then
+        pythonexecutable=".venv/bin/python"
+    else
+        python -m venv .venv
+        echo "No Python virtual environment found"
+        exit 1
+    fi
+fi
+echo "upd dependencies"
+"$pythonexecutable" -m pip install -r requirements.txt
+echo "done"
+echo -
+echo -
+
+
+"$pythonexecutable" -m src.launcher --program gitgui --working-tree-folder "tests-real-sensitive-data/my-test-project" --git-repo-folder "tests-real-sensitive-data/test-project-repo"

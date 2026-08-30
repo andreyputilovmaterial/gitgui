c52e28c24db64aa89fe1e3032f4c9ef84255332a
The-city-not-present <23407369+The-city-not-present@users.noreply.github.com>
2026-08-30 05:29:52 +0300

    webserve module updates, and logging

--- src/webserver_engine/helper_logger_funcs.py
+++ src/webserver_engine/helper_logger_funcs.py
@@ -1,12 +1,16 @@
-
-# aaahhh stupid
-
-
-import sys, traceback, io
+from io import StringIO
+# import logging
 import re
+import sys, traceback
+from dataclasses import dataclass
+from queue import Queue
+from typing import Any
 
-# from .globals import config
-config = {}
+from threading import Thread
+
+
+# logger = logging.getLogger(__name__)
+
 
 # STDOUT_COLOR_RED = "\033[91m"
 STDOUT_COLOR_RED = "\033[31m"
@@ -14,39 +18,97 @@ STDOUT_COLOR_RESET = "\033[0m"
 STDOUT_COLOR_GREEN = "\033[32m"
 
 
-def _err(*ars):
+@dataclass
+class Record:
+    message: Any
+    file: Any
+
+
+# TODO: use logging module
+# def configure_logging(script_name: str = "webserve") -> logging.LoggerAdapter:
+#     """
+#     Configure application-wide logging and return a logger carrying
+#     the script name as contextual information.
+#     """
+#     logging.basicConfig(
+#         level=logging.INFO,
+#         format="%(asctime)s %(levelname)s [%(script_name)s] %(message)s",
+#     )
+#
+#     return logging.LoggerAdapter(
+#         logger,
+#         {"script_name": script_name},
+#     )
+
+
+
+def worker_printer_loop(queue,_config):
+    while True:
+        record = queue.get()
+        print(
+            record.message,
+            file = record.file,
+            flush = True,
+        )
+
+
+def _err(*args):
     raise Exception(*args)
 
-def print_console(msg):
-    return print(f'{config.get("script_name","webserve")}: {msg}')
+class Logger:
+    def __init__(self, config: dict ):
+        self.config = {**config}
+        self.message_queue = Queue()
+        self.initiate_worker_loop(self.message_queue,self.config,)
 
-def print_console_green(msg):
-    return print(f'{config.get("script_name","webserve")}: {STDOUT_COLOR_GREEN}{msg}{STDOUT_COLOR_RESET}', flush=True)
+    def initiate_worker_loop(self, config, queue):
+        thread = Thread(
+            target = worker_printer_loop,
+            args   = ( config, queue, ),
+            daemon = True,
+        )
+        thread.start()
+        return thread
 
-def print_console_err(msg):
-    return print(f'{config.get("script_name","webserve")}: {STDOUT_COLOR_RED}{msg}{STDOUT_COLOR_RESET}',file=sys.stderr)
+    def log_network_request(self,message):
+        self.message_queue.put(Record(message=message,file=sys.stdout))
 
-def print_console_err_fulltrace(e):
-    print_console_err(f'\n{STDOUT_COLOR_RED}Error:\n{STDOUT_COLOR_RESET}Stack trace:\n')
-    traceback.print_exception(e,limit=20)
-    print_console_err('\n\n')
-    print_console_err('Error:\n')
-    print_console_err(f'{STDOUT_COLOR_RED}{e}{STDOUT_COLOR_RESET}')
-    print_console_err('')
+    def print_console(self,msg):
+        self.message_queue.put(Record(message=f'{self.config.get("script_name","webserve")}: {msg}',file=sys.stdout))
 
-def string_err_fulltrace(e):
-    buf = io.StringIO()
-    print('Error:\n',file=buf)
-    print(f'{STDOUT_COLOR_RED}{e}{STDOUT_COLOR_RESET}',file=buf)
-    print('',file=buf)
-    print(f'\n{STDOUT_COLOR_RED}{STDOUT_COLOR_RESET}Stack trace:\n',file=buf)
-    traceback.print_exception(e,limit=20,file=buf)
-    print('\n\n',file=buf)
-    txt = buf.getvalue()
-    ANSI_COLORS = {
-        STDOUT_COLOR_RED: '@STDOUT_COLOR_RED@',
-        STDOUT_COLOR_RESET: '@STDOUT_COLOR_RESET@',
-        STDOUT_COLOR_GREEN: '@STDOUT_COLOR_GREEN@',
-    }
-    ansi_re = re.compile("|".join(map(re.escape, ANSI_COLORS)))
-    return ansi_re.sub(lambda m: ANSI_COLORS[m.group()], txt)
+    def print_console_green(self,msg):
+        self.message_queue.put(Record(message=f'{self.config.get("script_name","webserve")}: {STDOUT_COLOR_GREEN}{msg}{STDOUT_COLOR_RESET}',file=sys.stdout))
+
+    def print_console_err(self,msg):
+        self.message_queue.put(Record(message=f'{self.config.get("script_name","webserve")}: {STDOUT_COLOR_RED}{msg}{STDOUT_COLOR_RESET}',file=sys.stderr))
+
+    def print_console_err_fulltrace(self,e):
+        buf = StringIO()
+        traceback.print_exception(e,limit=20, file=buf)
+        err_stacktrace = buf.getvalue()
+        txt = ''
+        txt += f'{STDOUT_COLOR_RED}Error:{STDOUT_COLOR_RESET}\n{STDOUT_COLOR_RED}Stack trace:'+f'{STDOUT_COLOR_RESET}\n'
+        txt += f'{STDOUT_COLOR_RED}'+''+f'{STDOUT_COLOR_RESET}\n'
+        txt += f'{STDOUT_COLOR_RED}'+'Error:'+f'{STDOUT_COLOR_RESET}\n'
+        txt += f'{STDOUT_COLOR_RED}{e}{STDOUT_COLOR_RESET}\n'
+        txt += f'\n'
+        txt += f'{STDOUT_COLOR_RED}'+err_stacktrace+f'{STDOUT_COLOR_RESET}\n\n'
+        self.message_queue.put(Record(message=txt,file=sys.stderr))
+
+    @staticmethod
+    def make_err_fulltrace_message(e):
+        buf = StringIO()
+        print('Error:\n',file=buf)
+        print(f'{STDOUT_COLOR_RED}{e}{STDOUT_COLOR_RESET}',file=buf)
+        print('',file=buf)
+        print(f'\n{STDOUT_COLOR_RED}{STDOUT_COLOR_RESET}Stack trace:\n',file=buf)
+        traceback.print_exception(e,limit=20,file=buf)
+        print('\n\n',file=buf)
+        txt = buf.getvalue()
+        ANSI_COLORS = {
+            STDOUT_COLOR_RED: '@STDOUT_COLOR_RED@',
+            STDOUT_COLOR_RESET: '@STDOUT_COLOR_RESET@',
+            STDOUT_COLOR_GREEN: '@STDOUT_COLOR_GREEN@',
+        }
+        ansi_re = re.compile("|".join(map(re.escape, ANSI_COLORS)))
+        return ansi_re.sub(lambda m: ANSI_COLORS[m.group()], txt)
--- src/webserver_engine/webserver.py
+++ src/webserver_engine/webserver.py
@@ -5,11 +5,7 @@ from dataclasses import dataclass
 
 
 
-
-from .helper_logger_funcs import print_console, print_console_err, print_console_err_fulltrace, print_console_green, string_err_fulltrace
-
-# sorry very stupid
-from .helper_logger_funcs import config as logging_helper_config
+from .helper_logger_funcs import Logger
 
 
 from .match_endpoints import get_matching_endpoint
@@ -24,14 +20,14 @@ class HTTP404(Exception):
 class HTTP403(Exception):
     """For HTTP 404"""
 
-def raise_err_404_not_found(*args,**argv):
+def raise_err_404_not_found(*_args,**_argv):
     raise HTTP404('404 not found')
 
 @dataclass
 class WebResponse:
     status_code: int
     content_type: str
-    body: str
+    body: str | bytes | None
     headers: list[tuple[str,str]]
     # cookies # can be passed in headers
     is_binary: bool = False
@@ -43,16 +39,13 @@ class WebResponse:
 class Webserver:
     def __init__(self,config,is_threading=True):
         self.endpoints = {}
-        self._config = config
+        self.config = config
         self.bind_host = config.get("http_host")
         self.port = config.get("http_port")
         self._is_threading_server = is_threading
-        # sorry very stupid
-        # from .logging_helper import config as logging_helper_config
-        for key, value in config.items():
-            logging_helper_config[key] = value
+        self.logger = Logger(self.config)
 
-    def assign_handlers(self,endpoints:dict={}):
+    def assign_handlers(self, endpoints: dict):
         self.endpoints = {**self.endpoints,**endpoints}
 
     def run(self):
@@ -66,7 +59,7 @@ class Webserver:
         server = cls((self.bind_host, self.port), self._get_handler(self.endpoints))
         if self._is_threading_server:
             server.daemon_threads = True
-        print_console_green(f'starting webserver at {self.bind_host}:{self.port}')
+        self.logger.print_console_green(f'starting webserver at {self.bind_host}:{self.port}')
         try:
             server.serve_forever()
         except KeyboardInterrupt:
@@ -88,7 +81,7 @@ class Webserver:
                     renderer = get_matching_endpoint(path,endpoints) or raise_err_404_not_found
                     assert callable(renderer), 'Whoops, renderer returned from get_matching_endpoint() must be callable'
 
-                    response = renderer(self, config=server._config)
+                    response: WebResponse = renderer(self, config=server.config)
 
                     if response.is_done:
                         # if all necessary headers and body were already sent - the renderer receives the handler instance and can send what is needed directly
@@ -96,7 +89,7 @@ class Webserver:
 
                     if response.is_stream:
                         assert response.is_binary, 'webserve: response.is_stream is only supported with response.is_binary'
-                        raise Exception('webserve: is_stream: not implemented');
+                        raise Exception('webserve: is_stream: not implemented')
 
                     if not response.content_type:
                         response.content_type = 'text/html'
@@ -132,7 +125,7 @@ class Webserver:
                     content = f'Can\'t find / no access: HTTP {statuscode}'.encode("utf-8")
                     renderer = endpoints.get(statuscode,None)
                     if renderer and send_body:
-                        response = renderer(self, config=server._config, msg = e)
+                        response = renderer(self, config=server.config, msg = e)
                         content = response.body
                         if not response.is_binary:
                             content = content.encode("utf-8")
@@ -142,10 +135,10 @@ class Webserver:
                     if send_body:
                         self.wfile.write(content)
                 except Exception as e:
-                    self.send_response(500)
+                    self.send_response(503)
                     self.send_header(f"Content-type", "text/plain; charset=utf-8")
                     self.end_headers()
-                    print_console_err_fulltrace(e)
+                    server.logger.print_console_err_fulltrace(e)
                     if send_body:
                         try:
                             err = f'{e}'
@@ -153,7 +146,7 @@ class Webserver:
                             err_txt = err
                             try:
                                 # several more levels to capture stacktrace, format it, convert "red" colors to spans with color red... If anything fails, there is a fallback to simpler way to just show the message
-                                err_full = string_err_fulltrace(e)
+                                err_full = Logger.make_err_fulltrace_message(e)
                                 err_txt = f'{err_full}'
                                 # err_html = '<br />'.join(html.escape(err_full).splitlines())
                                 # COLOR_MARKERS = {
@@ -166,20 +159,52 @@ class Webserver:
                                 # try:
                                 #     err_html = sanitize_dom('err err-stacktrace-container',err_html) # wrap results one more time to make sure all tags are closed
                                 # except Exception as ee:
-                                #     print_console_err_fulltrace(ee)
+                                #     server.logger.print_console_err_fulltrace(ee)
                                 #     pass
                             except Exception as ee:
-                                print_console_err_fulltrace(ee)
+                                server.logger.print_console_err_fulltrace(ee)
                                 pass
                             # self.wfile.write(("<html><body>"+err_html+"</body></html>").encode("utf-8"))
                             self.wfile.write((f"{err_txt}").encode("utf-8"))
                         except Exception as ee:
-                            print_console_err_fulltrace(ee)
+                            server.logger.print_console_err_fulltrace(ee)
                             # print fallback
                             # err_html = "error processing request"
                             err_txt = "error processing request"
                             self.wfile.write((f"{err_txt}").encode("utf-8"))
                             # self.wfile.write(("<html><body>"+err_html+"</body></html>").encode("utf-8"))
+
+            def log_message(self, format, *args):
+                """http.server logging fn, updated so that status column is aligned in one column after timestamp,
+so that it's easier to see 5xx codes; also have colors added.
+
+That gives you a log where 500/503 etc. immediately jump out in red, while 4xx are yellow.
+
+One caveat: log_message() is also used for things other than normal access logs, so if you
+have custom handlers emitting messages through it, you'd want to handle those separately.
+For ordinary http.server request logging, though, this works cleanly.
+                """
+                request = args[0]
+                status = int(args[1])
+                timestamp = self.log_date_time_string()
+
+                if status >= 500:
+                    color = "\033[31m"  # red
+                elif status >= 400:
+                    color = "\033[33m"  # yellow
+                elif status >= 300:
+                    color = "\033[36m"  # cyan
+                else:
+                    color = "\033[32m"  # green
+
+                reset = "\033[0m"
+
+                server.logger.log_network_request(
+                    f"{self.address_string()} - - "
+                    f"[{timestamp}] "
+                    f"{color}[{status:03d}]{reset} "
+                    f'"{request}"'
+                )
 
             def __getattr__(self, name):
                 if name.startswith("do_"):
